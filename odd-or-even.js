85ea1c0919fc96b63b88b38c8ed17bb7268f034c
Himalaya <yugmani@yahoo.com>
2020-05-13 23:50:59 -0700

    testing odd or even success

---
+++ odd-or-even.js
@@ -0,0 +1,10 @@
+// Write code to return the string "odd" if `num` is an odd number
+// and return the string "even" if `num` is an even number
+
+var oddOrEven = function(num) {
+    var remainder = num % 2;
+    if(remainder === 0){
+        return "even";
+    }
+    else return "odd";
+};
